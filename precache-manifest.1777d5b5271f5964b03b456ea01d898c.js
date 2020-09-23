73877d4dda2d0f7c2664aeb8d79b0e1cc8e1620c
AY-BAMZ <aybamz181@gmail.com>
2020-09-23 11:54:52 +0100

    Updates

---
+++ precache-manifest.1777d5b5271f5964b03b456ea01d898c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e28436560d721aedf0c7acaa80398c",
+    "url": "/AY-BAMZ/originfoods.git/index.html"
+  },
+  {
+    "revision": "006b1dc02fbfee77f022",
+    "url": "/AY-BAMZ/originfoods.git/static/css/2.2fc483f1.chunk.css"
+  },
+  {
+    "revision": "6b55bc9471ffe3d5d88a",
+    "url": "/AY-BAMZ/originfoods.git/static/css/main.a1804476.chunk.css"
+  },
+  {
+    "revision": "006b1dc02fbfee77f022",
+    "url": "/AY-BAMZ/originfoods.git/static/js/2.08e47ae3.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/AY-BAMZ/originfoods.git/static/js/2.08e47ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b55bc9471ffe3d5d88a",
+    "url": "/AY-BAMZ/originfoods.git/static/js/main.755c8781.chunk.js"
+  },
+  {
+    "revision": "2aba1d48282d0c181c71",
+    "url": "/AY-BAMZ/originfoods.git/static/js/runtime-main.395a444a.js"
+  },
+  {
+    "revision": "17a094a373575f073504b07a07ec590c",
+    "url": "/AY-BAMZ/originfoods.git/static/media/facebook.17a094a3.svg"
+  },
+  {
+    "revision": "2371f4293673e26527d53c626b832912",
+    "url": "/AY-BAMZ/originfoods.git/static/media/instagram.2371f429.svg"
+  },
+  {
+    "revision": "609f7bf42f11802b923234fa58ca5c5b",
+    "url": "/AY-BAMZ/originfoods.git/static/media/logo.609f7bf4.svg"
+  },
+  {
+    "revision": "4c47721daf8c312effe677be39b720da",
+    "url": "/AY-BAMZ/originfoods.git/static/media/twitter.4c47721d.svg"
+  },
+  {
+    "revision": "36d3147deabb995fb114b88f9bf220b7",
+    "url": "/AY-BAMZ/originfoods.git/static/media/whatsapp.36d3147d.svg"
+  }
+]);
